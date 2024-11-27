3811eb6acc9f6b4c38e9b1466b6fca2bc48ef061
Irina <Irisha.Kalagova@yandex.ru>
2024-11-27 21:30:36 +0300

    add comment

---
+++ app/src/main/java/com/example/tasktimer/view/AlgorithmFragment.kt
@@ -0,0 +1,36 @@
+package com.example.tasktimer.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tasktimer.R
+
+class AlgorithmFragment : Fragment() {
+
+    private lateinit var viewModel: AlgorithmViewModel
+    private lateinit var adapter: AlgorithmAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_algorithms, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        adapter = AlgorithmAdapter()
+        recyclerView.adapter = adapter
+
+        // Загружаем данные
+        viewModel.loadAlgorithms()
+        adapter.submitList(viewModel.algorithms)
+    }
+}
