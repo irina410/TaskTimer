b1b70c615719c7e52c96fd0eb8b8142f1bd02f16
Irina <Irisha.Kalagova@yandex.ru>
2025-04-01 08:39:37 +0300

    update task after update algorithm (fix totalTime)

--- app/src/main/java/com/example/tasktimer/model/Algorithm.kt
+++ app/src/main/java/com/example/tasktimer/model/Algorithm.kt
@@ -9,6 +9,6 @@ data class Algorithm(
     fun recalculateTotalTime() {
         totalTime = subtasks.sumOf { it.duration }
     }
-    fun copy() = Algorithm(name, subtasks.toList())
+    fun copy() = Algorithm(name, subtasks.toList(), totalTime)
 }
 
